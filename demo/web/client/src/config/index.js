8b9680468d33dc444f5d49692d9d828c859bce67
Nic Raboy <nraboy@gmail.com>
2017-10-10 16:17:08 -0700

    the analytics line chart now works with the fixed cbas query. it will populate on page load. still need to fix it so the data is dependent on what is entered via a form

--- demo/web/client/src/config/index.js
+++ demo/web/client/src/config/index.js
@@ -1,6 +1,7 @@
 export default {
   id: '0f62b5cb-3735-45f6-8ee9-deaeee7f308a',
-  serverURI: 'http://ec2-52-11-217-155.us-west-2.compute.amazonaws.com:3000',
+  // serverURI: 'http://ec2-52-11-217-155.us-west-2.compute.amazonaws.com:3000',
+  serverURI: 'http://localhost:3000',
   fixedLayout: false,
   hideLogoOnMobile: false
 }
