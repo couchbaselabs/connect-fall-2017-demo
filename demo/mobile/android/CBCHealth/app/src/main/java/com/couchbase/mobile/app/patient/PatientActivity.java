728d3da4ece7554c292190f26db5959436a9490a
hideki <hideki@couchbase.com>
2017-10-12 14:57:43 -0700

    Polished the patient profile ui

--- demo/mobile/android/CBCHealth/app/src/main/java/com/couchbase/mobile/app/patient/PatientActivity.java
+++ demo/mobile/android/CBCHealth/app/src/main/java/com/couchbase/mobile/app/patient/PatientActivity.java
@@ -1,17 +1,11 @@
 package com.couchbase.mobile.app.patient;
 
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.view.View;
-import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
 import com.couchbase.lite.Array;
-import com.couchbase.lite.DatabaseChange;
-import com.couchbase.lite.DatabaseChangeListener;
 import com.couchbase.lite.Dictionary;
 import com.couchbase.lite.Document;
 import com.couchbase.lite.DocumentChange;
@@ -24,10 +18,11 @@ import com.couchbase.mobile.database.CBLite;
 import java.util.ArrayList;
 import java.util.List;
 
-public class PatientActivity extends AppCompatActivity implements DocumentChangeListener , DatabaseChangeListener {
+public class PatientActivity extends AppCompatActivity implements DocumentChangeListener {
     public static final String TAG = PatientActivity.class.getSimpleName();
 
-    ArrayAdapter<String> itemsAdapter = null;
+    ItemsAdapter adapter = null;
+    String docID = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -36,38 +31,43 @@ public class PatientActivity extends AppCompatActivity implements DocumentChange
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
+        setTitle("Patient Profile");
 
-        Document doc = CBLite.getInstance().getDatabase().getDocument(Runtime.getPatientID());
-        Log.e(TAG, "doc -> %s", doc.toMap());
-
-        CBLite.getInstance().getDatabase().addChangeListener(doc.getId(), this);
-        //CBLite.getInstance().getDatabase().addChangeListener(this);
-
-        List<String> items = getPatientProfileList(doc);
-        itemsAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, items);
+        adapter = new ItemsAdapter(this, new ArrayList<Item>());
         ListView listView = findViewById(R.id.lvItems);
-        listView.setAdapter(itemsAdapter);
+        listView.setAdapter(adapter);
     }
 
-    List<String> getPatientProfileList(Document doc){
-        List<String> items = new ArrayList<>();
-        items.add(getName(doc));
-        items.add(getGender(doc));
-        items.add(getBirthDate(doc));
-        items.add(getAddress(doc));
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Document doc = CBLite.getInstance().getDatabase().getDocument(Runtime.getPatientID());
+        if (doc != null) {
+            docID = doc.getId();
+            CBLite.getInstance().getDatabase().addChangeListener(docID, this);
+            Log.e(TAG, "doc -> %s", doc.toMap());
+            adapter.addAll(getPatientProfileList(doc));
+        }
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        if (docID != null)
+            CBLite.getInstance().getDatabase().removeChangeListener(docID, this);
+    }
+
+    List<Item> getPatientProfileList(Document doc) {
+        List<Item> items = new ArrayList<>();
+        items.add(new Item("Name", getName(doc)));
+        items.add(new Item("Gender", getGender(doc)));
+        items.add(new Item("Birth Date", getBirthDate(doc)));
+        items.add(new Item("Address", getAddress(doc)));
         items.addAll(getExtension(doc));
         return items;
     }
 
-    String getName(Document doc){
+    String getName(Document doc) {
         Array names = doc.getArray("name");
         Dictionary name = names.getDictionary(0);
         Array family = name.getArray("family");
@@ -75,26 +75,26 @@ public class PatientActivity extends AppCompatActivity implements DocumentChange
         return given.getString(0) + " " + family.getString(0);
     }
 
-    String getGender(Document doc){
+    String getGender(Document doc) {
         Dictionary gender = doc.getDictionary("gender");
         return gender.getString("text");
     }
 
-    String getBirthDate(Document doc){
+    String getBirthDate(Document doc) {
         return doc.getString("birthDate");
     }
 
-    String getAddress(Document doc){
+    String getAddress(Document doc) {
         Dictionary gender = doc.getDictionary("address");
         return gender.getString("text");
     }
 
-    List<String> getExtension(Document doc){
-        List<String> list = new ArrayList<>();
+    List<Item> getExtension(Document doc) {
+        List<Item> list = new ArrayList<>();
         Dictionary extension = doc.getDictionary("extension");
-        if(extension!=null){
-            for(String key: extension.getKeys()){
-                list.add(key + ": " + extension.getObject(key));
+        if (extension != null) {
+            for (String key : extension.getKeys()) {
+                list.add(new Item(key, extension.getObject(key).toString()));
             }
         }
         return list;
@@ -104,14 +104,10 @@ public class PatientActivity extends AppCompatActivity implements DocumentChange
     public void changed(DocumentChange change) {
         Log.e(TAG, "DocumentChange -> %s", change.getDocumentID());
         Document doc = CBLite.getInstance().getDatabase().getDocument(Runtime.getPatientID());
-        Log.e(TAG, "doc -> %s", doc.toMap());
-
-        this.itemsAdapter.clear();
-        this.itemsAdapter.addAll(getPatientProfileList(doc));
-    }
-
-    @Override
-    public void changed(DatabaseChange change) {
-        Log.e(TAG, "DatabaseChange -> %s", change.getDocumentIDs());
+        if (doc != null) {
+            Log.e(TAG, "doc -> %s", doc.toMap());
+            this.adapter.clear();
+            this.adapter.addAll(getPatientProfileList(doc));
+        }
     }
 }
