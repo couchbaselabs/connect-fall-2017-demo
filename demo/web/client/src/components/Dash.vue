abc7c385ce47a4b96914bd2fb9e00ac392f251af
Hod Greeley <hod@greeley.org>
2017-10-15 23:24:22 -0700

    Trigger alert from temp reading.

--- demo/web/client/src/components/Dash.vue
+++ demo/web/client/src/components/Dash.vue
@@ -17,104 +17,39 @@
         <!-- Navbar Right Menu -->
         <div class="navbar-custom-menu" style="margin-right:32px;">
           <ul class="nav navbar-nav">
-            <!-- Messages-->
-            <li class="dropdown messages-menu">
+            <!-- Alerts Menu -->
+            <li class="dropdown tasks-menu">
               <a href="javascript:;" class="dropdown-toggle" data-toggle="dropdown">
-                <i class="fa fa-2x fa-envelope-o"></i>
-                <span class="label label-success">{{ userInfo.messages | count }}</span>
+                <i class="fa fa-2x fa-bell-o"></i>
+                <span class="label label-danger">{{ userInfo.alerts | count }}</span>
               </a>
               <ul class="dropdown-menu">
-                <li class="header">You have {{ userInfo.messages | count }} message(s)</li>
-                <li v-if="userInfo.messages.length > 0">
-                  <!-- inner menu: contains the messages -->
+                <li class="header">You have {{ userInfo.alerts | count }} alert(s)</li>
+                <li v-if="userInfo.alerts.length > 0">
+                  <!-- Inner Menu: contains the alerts -->
                   <ul class="menu">
                     <li>
-                      <!-- start message -->
+                      <!-- start alert -->
                       <a href="javascript:;">
-                        <!-- Message title and timestamp -->
                         <h4>
-                          Support Team
+                          Patient Alert
                           <small>
                             <i class="fa fa-2x fa-clock-o"></i> 5 mins</small>
                         </h4>
                         <!-- The message -->
-                        <p>Why not consider this a test message?</p>
+                        <p>Temperature Spike <em>{{ userInfo.alerts[0].value.toFixed(1) }}</em></p>
+                        <button v-on:click="showPatient()" type="button" class="btn btn-block btn-danger btn-xs">Patient Detail</button>
+                      </a>
                       </a>
                     </li>
-                    <!-- end message -->
-                  </ul>
-                  <!-- /.menu -->
-                </li>
-                <li class="footer" v-if="userInfo.messages.length > 0">
-                  <a href="javascript:;">See All Messages</a>
-                </li>
-              </ul>
-            </li>
-            <!-- /.messages-menu -->
-
-            <!-- Notifications Menu -->
-            <li class="dropdown notifications-menu">
-              <a href="javascript:;" class="dropdown-toggle" data-toggle="dropdown">
-                <i class="fa fa-2x fa-bell-o"></i>
-                <span class="label label-warning">{{ userInfo.notifications | count }}</span>
-              </a>
-              <ul class="dropdown-menu">
-                <li class="header">You have {{ userInfo.notifications | count }} notification(s)</li>
-                <li v-if="userInfo.notifications.length > 0">
-                  <!-- Inner Menu: contains the notifications -->
-                  <ul class="menu">
-                    <li>
-                      <!-- start notification -->
-                      <a href="javascript:;">
-                        <i class="fa fa-users text-aqua"></i> 5 new members joined today
-                      </a>
-                    </li>
-                    <!-- end notification -->
+                    <!-- end alert -->
                   </ul>
                 </li>
-                <li class="footer" v-if="userInfo.notifications.length > 0">
+                <li class="footer" v-if="userInfo.alerts.length > 0">
                   <a href="javascript:;">View all</a>
                 </li>
               </ul>
             </li>
-
-            <!-- Tasks Menu -->
-            <li class="dropdown tasks-menu">
-              <a href="javascript:;" class="dropdown-toggle" data-toggle="dropdown">
-                <i class="fa fa-2x fa-flag-o"></i>
-                <span class="label label-danger">{{ userInfo.tasks | count }} </span>
-              </a>
-              <ul class="dropdown-menu">
-                <li class="header">You have {{ userInfo.tasks | count }} task(s)</li>
-                <li v-if="userInfo.tasks.length > 0">
-                  <!-- Inner menu: contains the tasks -->
-                  <ul class="menu">
-                    <li>
-                      <!-- Task item -->
-                      <a href="javascript:;">
-                        <!-- Task title and progress text -->
-                        <h3>
-                          Design some buttons
-                          <small class="pull-right">20%</small>
-                        </h3>
-                        <!-- The progress bar -->
-                        <div class="progress xs">
-                          <!-- Change the css width attribute to simulate progress -->
-                          <div class="progress-bar progress-bar-aqua" style="width: 20%" role="progressbar" aria-valuenow="20" aria-valuemin="0" aria-valuemax="100">
-                            <span class="sr-only">20% Complete</span>
-                          </div>
-                        </div>
-                      </a>
-                    </li>
-                    <!-- end task item -->
-                  </ul>
-                </li>
-                <li class="footer" v-if="userInfo.tasks.length > 0">
-                  <a href="javascript:;">View all tasks</a>
-                </li>
-              </ul>
-            </li>
-
           </ul>
         </div>
       </nav>
@@ -125,8 +60,6 @@
     <!-- Content Wrapper. Contains page content -->
     <div class="content-wrapper">
       <!-- Content Header (Page header) -->
-
-
       <router-view></router-view>
     </div>
     <!-- /.content-wrapper -->
@@ -141,6 +74,7 @@
 </template>
 
 <script>
+import EventBus from '../event-bus'
 import faker from 'faker'
 import { mapState } from 'vuex'
 import config from '../config'
@@ -177,9 +111,31 @@ export default {
     }
   },
   methods: {
+    showPatient () {
+      this.$store.commit('SET_ALERT', [])
+      this.$router.push({ path: '/patient' })
+    },
     changeloading () {
       this.$store.commit('TOGGLE_SEARCHING')
     }
+  },
+  mounted () {
+    this.lastSampleTime = 0
+
+    EventBus.$on('update', data => {
+      data.values.forEach(sample => {
+        let recordedAt = new Date(sample.recordedAt)
+
+        if (this.lastSampleTime >= recordedAt.valueOf()) return
+
+        this.lastSampleTime = recordedAt.valueOf()
+
+        if (sample.value > 82) this.$store.commit('SET_ALERT', [ sample ])
+      }, this)
+    })
+  },
+  destroyed () {
+    EventBus.$off('update')
   }
 }
 </script>
@@ -211,8 +167,8 @@ text-align: left;
 .skin-blue .main-header .logo img {
 padding: 0;
 margin: 0;
-
 }
+
 .skin-blue .main-header .navbar {
 background-color: #3575C6!important;
 }
