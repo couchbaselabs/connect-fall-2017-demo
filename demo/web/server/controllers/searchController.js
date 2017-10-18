a4fc8a8e11d21964381036a64c694ad84dfa12a2
Hod Greeley <hod@greeley.org>
2017-10-17 22:42:05 -0700

    Changes to performa analytics on social media fields added.

--- demo/web/server/controllers/searchController.js
+++ demo/web/server/controllers/searchController.js
@@ -2,6 +2,9 @@ var express = require('express');
 var randomHexColor = require('random-hex-color');
 var router = express.Router();
 
+// Based on Couchbase colors (TM)
+let palette = [ '#0074e0', '#f0ce0f', '#b26cda', '#00b6bd', '#00a1db', '#eb242a', '#fd9d0d' ];
+
 exports.encounters = async function(req, res, next) {
   let couchbase = req.app.locals.couchbase;
   let bucket = req.app.locals.bucket;
@@ -137,7 +140,7 @@ group by substring(e.period.`start`, 1, 7) as year_month, p.maritalStatus.text a
                     "AND GET_DATE_FROM_DATETIME(DATETIME(e.period.`start`)) > DATE('2007-10-01') ";
 
     if(req.query.gender != "both") {
-                    statement += "AND p.gender = '" + req.query.gender + "' ";
+                    statement += "AND p.gender = lower('" + req.query.gender + "') ";
     }
 
     statement +=    "AND c.code.text = '" + req.query.code + "' " +
@@ -195,19 +198,102 @@ exports.analyticsDetails = async function(req, res, next) {
                     "WHERE p.id = substring_after(e.subject.reference, 'uuid:') " +
                     "AND e.id = substring_after(c.context.reference, 'uuid:') ";
 
-    if(req.query.gender != "both") {
-                    statement += "AND p.gender = '" + req.query.gender + "' ";
+    if (req.query.gender.toLowerCase() != "all") {
+                    statement += `AND p.gender = '${req.query.gender.toLowerCase()}' `;
     }
 
-    statement +=    "AND c.code.text = '" + req.query.code + "' " +
+    statement +=    "AND c.code.text = '" + req.query.diagnosis + "' " +
                     "AND GET_YEAR(DATETIME(e.period.`start`)) - GET_YEAR(DATE(p.birthDate)) BETWEEN " + req.query.min_age + " AND " + req.query.max_age + " " +
                     "AND SUBSTRING(e.period.`start`, 1, 7) = '" + req.query.year_month + "' " +
                     "LIMIT 20";
+
     var query = CbasQuery.fromString(statement);
+
     cluster.query(query, (error, result) => {
         if(error) {
             return res.status(500).send({ code: error.code, message: error.message });
         }
         res.send(result);
     });
+}
+
+exports.analyticsSocial = async function(req, res, next) {
+  let couchbase = req.app.locals.couchbase;
+  let cluster = req.app.locals.cluster;
+  let CbasQuery = couchbase.CbasQuery;
+  
+  let query = `SELECT year_month, Facebook, WhatsApp, Snapchat, count(p.id) as patient_count
+                 FROM condition_archive c, patient_archive p
+                 WHERE substring_after(c.subject.reference, "uuid:") = meta(p).id
+                 AND c.code.text = '${req.query.diagnosis}'
+                 AND date(c.assertedDate) > date('2007-01-01') `;
+  
+  if (req.query.gender != 'All') {
+    query += `AND p.gender = '${req.query.gender.toLowerCase()}' `;
+  }
+
+  query += `AND lower(p.address[0].city) = lower('${req.query.city}')
+            GROUP BY
+              substring(c.assertedDate, 1, 7) as year_month,
+              p.telecom[0].facebook is not unknown as Facebook,
+              p.telecom[0].whatsapp is not unknown as WhatsApp,
+              p.telecom[0].snapchat is not unknown as Snapchat;`
+
+  query = CbasQuery.fromString(query);
+
+  cluster.query(query, (error, result) => {
+    if (error) {
+      return res.status(500).send({ code: error.code, message: error.message });
+    }
+
+    let stats = { 'None': {}, 'Facebook': {}, 'Snapchat': {}, 'WhatsApp': {}};
+    let datasets = [];
+    let labels = [];
+
+    for (const record of result) {
+      let found = false;
+
+      labels.push(record.year_month);
+
+      for (const key in stats) {
+        if (record[key]) {
+          stats[key][record.year_month] = record.patient_count;
+
+          found = true;
+        }
+      }
+
+      if (!found) stats['None'][record.year_month] = record.patient_count;
+    }
+    
+    labels = [...new Set(labels)].sort();
+
+    let knife = 0;
+
+    for (const key in stats) {
+      if (stats.hasOwnProperty(key)) {
+        var entries = [];
+
+        for (let nn = 0; nn < labels.length; ++nn) {
+          entries.push(stats[key][labels[nn]]);
+        }
+
+        datasets.push({
+            data: entries,
+            label: key,
+            fill: false,
+            backgroundColor: 'rgba(0, 0, 0, 0)',
+            borderColor: palette[knife],
+            pointBackgroundColor: palette[knife]
+        });
+      }
+
+      knife = (knife + 1) % palette.length;
+    }
+
+    res.send({
+        labels: labels,
+        datasets: datasets
+    });
+  });
 }
