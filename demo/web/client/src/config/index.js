0c5ee57f1d4de2ddcd37dc37bcbc7c90e7ad3001
Hod Greeley <hod@greeley.org>
2018-02-22 14:11:00 -0800

    Update server port

--- demo/web/client/src/config/index.js
+++ demo/web/client/src/config/index.js
@@ -1,7 +1,8 @@
 export default {
   id: '0f62b5cb-3735-45f6-8ee9-deaeee7f308a',
   // serverURI: 'http://ec2-52-11-217-155.us-west-2.compute.amazonaws.com:3000',
-  serverURI: 'http://localhost:3000',
+  // serverURI: 'http://localhost:3000',
+  serverURI: 'http://localhost:8080',
   fixedLayout: false,
   hideLogoOnMobile: false
 }
