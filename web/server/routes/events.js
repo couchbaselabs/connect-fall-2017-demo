9a73c9bbe8dbabfb8f53c6214c1acf21608deb0f
Hod Greeley <hod@greeley.org>
2018-06-11 14:41:04 -0700

    Rework server side events to pull last temp readings as well as setting up to receive new data pushes

--- web/server/routes/events.js
+++ web/server/routes/events.js
@@ -1,13 +1,31 @@
-// I require sleep
-const sleep = require('../utils/sleep');
 const N1qlQuery = require('../utils/n1ql');
 const express = require('express');
 const router = express.Router();
 
 let sse = {};
 
-router.get('/:id', function(req, res, next) {
+router.get('/:id', async function(req, res, next) {
   sse[req.params.id] = res.sse;
+
+  // Send latest readings.  New readings will be pushed.
+  let couchbase = req.app.locals.couchbase;
+  let bucket = req.app.locals.bucket;
+  let queryPromise = Promise.promisify(bucket.query, { context: bucket });
+  let n1ql = `SELECT valueQuantity.\`value\`, issued as recordedAt FROM ${bucket._name} WHERE subject.reference = 'urn:uuid:${req.params.id}' ORDER BY recordedAt DESC LIMIT 12;`;
+
+  let query = couchbase.N1qlQuery.fromString(n1ql);
+  
+  await queryPromise(query)
+  .then(rows => {
+    if (rows.length < 1) return;
+
+    res.sse('event: update\ndata: { "values": ');
+    res.sse(JSON.stringify(rows.reverse()));      
+    res.sse(' }\n\n');
+  })
+  .catch(err => {
+    console.log(err);
+  })
 });
 
 router.post('/:id', function(req, res, next) {
